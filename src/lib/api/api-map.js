7d5c5ee4e264d1a40145f9977c2f1bbf8fa81b90
Lucho Suarez <luis.suarez@rrpartners.com>
2017-12-11 10:47:03 -0800

    Added the api wrapper

---
+++ src/lib/api/api-map.js
@@ -0,0 +1,8 @@
+const prefixURL = '/wp-json/CHANGE_THIS_NAME/v1'
+
+export default {
+  // WP API MAPPER
+  // menus                : '/wp-api-menus/v2/menus/:menu_id',
+  page                 : prefixURL + '/page/:slug',
+
+}
