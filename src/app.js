54dc41998ced1bbec6b3e8ae58dffdbea4041484
Lucho Suarez <luis.suarez@rrpartners.com>
2018-02-04 00:23:24 -0800

    Adding more configuration for login

--- src/app.js
+++ src/app.js
@@ -1,22 +1,34 @@
-import React, { Component }      from 'react'
-import { BrowserRouter, Switch } from 'react-router-dom'
+import React, {Component} from 'react'
+import {BrowserRouter, Switch} from 'react-router-dom'
 import {
   AppBar,
   Drawer,
   IconButton,
   ListItem,
   Toolbar,
-  Typography,
-}                                   from 'material-ui'
-import MenuIcon                     from 'material-ui-icons/Menu'
-import { RouteFunctor, RouteActor } from './routes'
+  Typography
+} from 'material-ui'
+import MenuIcon from 'material-ui-icons/Menu'
+import {RouteFunctor, RouteActor} from './routes'
 
 class App extends Component {
   state = {
     open: false
   }
 
-  _handleToggle = () => this.setState({open: !this.state.open});
+  componentDidMount() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/'
+          })
+        }
+      })
+    }
+  }
+
+  _handleToggle = () => this.setState({open: !this.state.open})
 
   render() {
     return (
@@ -24,7 +36,10 @@ class App extends Component {
         <div className="App">
           <AppBar position="static" className="app-bar">
             <Toolbar>
-              <IconButton onTouchTap={this._handleToggle} color="contrast" aria-label="Menu">
+              <IconButton
+                onTouchTap={this._handleToggle}
+                color="contrast"
+                aria-label="Menu">
                 <MenuIcon />
               </IconButton>
               <Typography type="title" color="inherit" style={{flex: 1}}>
@@ -32,24 +47,22 @@ class App extends Component {
               </Typography>
             </Toolbar>
           </AppBar>
-          <Drawer docked={false} open={this.state.open} onRequestClose={this._handleToggle}>
+          <Drawer
+            docked={false}
+            open={this.state.open}
+            onRequestClose={this._handleToggle}>
             <ListItem>Menu Item</ListItem>
             <ListItem>Menu Item</ListItem>
           </Drawer>
           <Switch>
-            {
-              RouteFunctor.map( (route,key) => (
-                <RouteActor
-                  key={key}
-                  {...route}
-                />
-              ))
-            }
+            {RouteFunctor.map((route, key) => (
+              <RouteActor key={key} {...route} />
+            ))}
           </Switch>
         </div>
       </BrowserRouter>
-    );
+    )
   }
 }
 
-export default App;
+export default App
